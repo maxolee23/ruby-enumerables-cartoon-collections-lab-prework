84dcb4259dccff1211f3504a786a20e208abdf0c
Max Lee <maxolee23@gmail.com>
2019-07-16 17:41:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
