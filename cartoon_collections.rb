66e3e0dccdb4ab01b724145ef7fc053ec77ec4ac
Max Lee <maxolee23@gmail.com>
2019-07-16 17:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,12 @@ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
 def roll_call_dwarves(array)
-
+i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
+end
 end
 
 def summon_captain_planet# code an argument here
